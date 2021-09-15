744dd95e989055241963a58895c885539c140e44
chalani <chalanicodes@gmail.com>
2021-09-15 22:17:51 +1000

    added chalani.php

---
+++ chalani.php
@@ -0,0 +1,21 @@
+<?php
+
+class Employee {
+
+}
+
+class Customer {
+
+}
+
+$mike = new Employee;
+$richard = new Employee;
+$david = new Customer;
+
+echo get_class($mike);
+echo '<br>';
+echo get_class($david);
+echo '<br>';
+echo 'My name is Chalani';
+
+  ?>
